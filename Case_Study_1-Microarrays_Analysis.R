43479badd7cab68a7e8d7ed695285fee16bcba41
alexsanchezpla <asanchez@ub.edu>
2021-04-14 16:19:34 +0200

    update R code

--- Case_Study_1-Microarrays_Analysis.R
+++ Case_Study_1-Microarrays_Analysis.R
@@ -1,4 +1,4 @@
-## ----setup, include=FALSE---------------------------------------------------
+## ----setup, include=FALSE------------------------------------------------------------------------------------------------------
 library(knitr)
 knitr::opts_chunk$set(echo = TRUE, message = FALSE, warning = FALSE, 
                       comment = NA, prompt = TRUE, tidy = FALSE, 
@@ -7,7 +7,7 @@ knitr::opts_chunk$set(echo = TRUE, message = FALSE, warning = FALSE,
 Sys.setlocale("LC_TIME", "C")
 
 
-## ----echo=FALSE-------------------------------------------------------------
+## ----echo=FALSE----------------------------------------------------------------------------------------------------------------
 if(!(require(printr))) {
   install.packages(
     'printr',
@@ -17,34 +17,34 @@ if(!(require(printr))) {
 }
 
 
-## ----dataset, fig.cap="A simplified view of a gene expression matrix", echo=FALSE----
+## ----dataset, fig.cap="A simplified view of a gene expression matrix", echo=FALSE----------------------------------------------
 knitr::include_graphics("figures/Figure1.jpg")
 
 
-## ----MDAProcess, fig.cap="The microarray data analysis process", echo=FALSE----
+## ----MDAProcess, fig.cap="The microarray data analysis process", echo=FALSE----------------------------------------------------
 knitr::include_graphics("figures/Figure2.png")
 
 
-## ----CreateFolders, warning=FALSE, eval=FALSE-------------------------------
+## ----CreateFolders, warning=FALSE, eval=FALSE----------------------------------------------------------------------------------
 ## setwd(".")
 ## dir.create("data")
 ## dir.create("results")
 
 
-## ----ReadTargets------------------------------------------------------------
+## ----ReadTargets---------------------------------------------------------------------------------------------------------------
 targets <- read.csv2("./data/targets.csv", header = TRUE, sep = ";") 
 knitr::kable(
   targets, booktabs = TRUE,
   caption = 'Content of the targets file used for the current analysis')
 
 
-## ----installBioC, message=FALSE, warning=FALSE, eval=FALSE------------------
+## ----installBioC, message=FALSE, warning=FALSE, eval=FALSE---------------------------------------------------------------------
 ## if (!requireNamespace("BiocManager", quietly = TRUE))
 ##     install.packages("BiocManager")
 ## BiocManager::install()
 
 
-## ----installPackages, message=FALSE, warning=FALSE, eval=FALSE--------------
+## ----installPackages, message=FALSE, warning=FALSE, eval=FALSE-----------------------------------------------------------------
 ## install.packages("knitr")
 ## install.packages("colorspace")
 ## install.packages("gplots")
@@ -68,7 +68,7 @@ knitr::kable(
 ## BiocManager::install("reactome.db")
 
 
-## ----ReadCELfiles, message=FALSE, results='hide', warning=FALSE-------------
+## ----ReadCELfiles, message=FALSE, results='hide', warning=FALSE----------------------------------------------------------------
 library(oligo)
 celFiles <- list.celfiles("./data", full.names = TRUE)
 library(Biobase)
@@ -78,14 +78,14 @@ my.targets <-read.AnnotatedDataFrame(file.path("./data","targets.csv"),
 rawData <- read.celfiles(celFiles, phenoData = my.targets)
 
 
-## ----ChangeName-------------------------------------------------------------
+## ----ChangeName----------------------------------------------------------------------------------------------------------------
 my.targets@data$ShortName->rownames(pData(rawData))
 colnames(rawData) <-rownames(pData(rawData)) 
 
 head(rawData)
 
 
-## ----QCRaw, message=FALSE, warning=FALSE, eval=FALSE------------------------
+## ----QCRaw, message=FALSE, warning=FALSE, eval=FALSE---------------------------------------------------------------------------
 ## library(arrayQualityMetrics)
 ## arrayQualityMetrics(rawData)
 
@@ -94,7 +94,7 @@ head(rawData)
 knitr::include_graphics("figures/Figure3.png")
 
 
-## ---------------------------------------------------------------------------
+## ------------------------------------------------------------------------------------------------------------------------------
 library(ggplot2)
 library(ggrepel)
 plotPCA3 <- function (datos, labels, factor, title, scale,colores, size = 1.5, glineas = 0.25) {
@@ -120,23 +120,23 @@ plotPCA3 <- function (datos, labels, factor, title, scale,colores, size = 1.5, g
   }
 
 
-## ----PCARaw, message=FALSE, fig.cap="Visualization of the two first Principal Components for raw data"----
+## ----PCARaw, message=FALSE, fig.cap="Visualization of the two first Principal Components for raw data"-------------------------
 plotPCA3(exprs(rawData), labels = targets$ShortName, factor = targets$Group, 
          title="Raw data", scale = FALSE, size = 3, 
          colores = c("red", "blue", "green", "yellow"))
 
 
-## ----BoxplotRaw, message=FALSE, fig.cap="Boxplot for arrays intensities (Raw Data)"----
+## ----BoxplotRaw, message=FALSE, fig.cap="Boxplot for arrays intensities (Raw Data)"--------------------------------------------
 boxplot(rawData, cex.axis=0.5, las=2,  which="all", 
          col = c(rep("red", 3), rep("blue", 3), rep("green", 3), rep("yellow", 3)),
          main="Distribution of raw intensity values")
 
 
-## ----Normalization----------------------------------------------------------
+## ----Normalization-------------------------------------------------------------------------------------------------------------
 eset_rma <- rma(rawData)
 
 
-## ----QCNorm, message=FALSE, warning=FALSE, eval=FALSE-----------------------
+## ----QCNorm, message=FALSE, warning=FALSE, eval=FALSE--------------------------------------------------------------------------
 ## arrayQualityMetrics(eset_rma, outdir = file.path("./results", "QCDir.Norm"), force=TRUE)
 
 
@@ -144,19 +144,19 @@ eset_rma <- rma(rawData)
 knitr::include_graphics("figures/Figure6.png")
 
 
-## ----fig:PCANorm, message=FALSE, fig.cap="Visualization of first two principal components for normalized data"----
+## ----fig:PCANorm, message=FALSE, fig.cap="Visualization of first two principal components for normalized data"-----------------
 plotPCA3(exprs(eset_rma), labels = targets$ShortName, factor = targets$Group, 
          title="Normalized data", scale = FALSE, size = 3, 
          colores = c("red", "blue", "green", "yellow"))
 
 
-## ----BoxplotNorm, message=FALSE, fig.cap="Distribution of  intensities for normalized data"----
+## ----BoxplotNorm, message=FALSE, fig.cap="Distribution of  intensities for normalized data"------------------------------------
 boxplot(eset_rma, cex.axis=0.5, las=2,  which="all", 
          col = c(rep("red", 3), rep("blue", 3), rep("green", 3), rep("yellow", 3)),
          main="Boxplot for arrays intensity: Normalized Data")
 
 
-## ----BatchDetection, message=FALSE, warning=FALSE---------------------------
+## ----BatchDetection, message=FALSE, warning=FALSE------------------------------------------------------------------------------
 #load the library
 library(pvca)
 pData(eset_rma) <- targets
@@ -180,7 +180,7 @@ new_values = round(values , 3)
 text(bp,pvcaObj$dat,labels = new_values, pos=3, cex = 0.7)
 
 
-## ----SDplot, fig.cap="Values of standard deviations allong all samples for all genes ordered from smallest to biggest"----
+## ----SDplot, fig.cap="Values of standard deviations allong all samples for all genes ordered from smallest to biggest"---------
 sds <- apply (exprs(eset_rma), 1, sd)
 sdsO<- sort(sds)
 plot(1:length(sdsO), sdsO, main="Distribution of variability for all genes",
@@ -189,7 +189,7 @@ plot(1:length(sdsO), sdsO, main="Distribution of variability for all genes",
 abline(v=length(sds)*c(0.9,0.95))
 
 
-## ----Filtering1, results='hide', message=FALSE------------------------------
+## ----Filtering1, results='hide', message=FALSE---------------------------------------------------------------------------------
 library(genefilter)
 library(mogene21sttranscriptcluster.db)
 annotation(eset_rma) <- "mogene21sttranscriptcluster.db"
@@ -199,34 +199,34 @@ filtered <- nsFilter(eset_rma,
                      filterByQuantile=TRUE, feature.exclude = "^AFFX")
 
 
-## ----FilterResults1, results='hide', echo=FALSE-----------------------------
+## ----FilterResults1, results='hide', echo=FALSE--------------------------------------------------------------------------------
 names(filtered)
 class(filtered$eset)
 
 
-## ----FilterResults2---------------------------------------------------------
+## ----FilterResults2------------------------------------------------------------------------------------------------------------
 print(filtered$filter.log)
 eset_filtered <-filtered$eset
 
 
-## ----SaveData1, results='hide', message=FALSE-------------------------------
+## ----SaveData1, results='hide', message=FALSE----------------------------------------------------------------------------------
 write.csv(exprs(eset_rma), file="./results/normalized.Data.csv")
 write.csv(exprs(eset_filtered), file="./results/normalized.Filtered.Data.csv")
 save(eset_rma, eset_filtered, file="./results/normalized.Data.Rda")
 
 
-## ----LoadSavedData----------------------------------------------------------
+## ----LoadSavedData-------------------------------------------------------------------------------------------------------------
 if (!exists("eset_filtered")) load (file="./results/normalized.Data.Rda")
 
 
-## ----DesignMatrix, message=FALSE--------------------------------------------
+## ----DesignMatrix, message=FALSE-----------------------------------------------------------------------------------------------
 library(limma)
 designMat<- model.matrix(~0+Group, pData(eset_filtered))
 colnames(designMat) <- c("KO.COLD", "KO.RT", "WT.COLD", "WT.RT")
 print(designMat)
 
 
-## ----setContrasts-----------------------------------------------------------
+## ----setContrasts--------------------------------------------------------------------------------------------------------------
 cont.matrix <- makeContrasts (KOvsWT.COLD = KO.COLD-WT.COLD,
                               KOvsWT.RT = KO.RT-WT.RT,
                               INT = (KO.COLD-WT.COLD) - (KO.RT-WT.RT),
@@ -234,7 +234,7 @@ cont.matrix <- makeContrasts (KOvsWT.COLD = KO.COLD-WT.COLD,
 print(cont.matrix)
 
 
-## ---- linearmodelfit--------------------------------------------------------
+## ---- linearmodelfit-----------------------------------------------------------------------------------------------------------
 library(limma)
 fit<-lmFit(eset_filtered, designMat)
 fit.main<-contrasts.fit(fit, cont.matrix)
@@ -242,22 +242,22 @@ fit.main<-eBayes(fit.main)
 class(fit.main)
 
 
-## ---- topTabs1--------------------------------------------------------------
+## ---- topTabs1-----------------------------------------------------------------------------------------------------------------
 topTab_KOvsWT.COLD <- topTable (fit.main, number=nrow(fit.main), coef="KOvsWT.COLD", adjust="fdr") 
 head(topTab_KOvsWT.COLD)
 
 
-## ---- topTabs2--------------------------------------------------------------
+## ---- topTabs2-----------------------------------------------------------------------------------------------------------------
 topTab_KOvsWT.RT <- topTable (fit.main, number=nrow(fit.main), coef="KOvsWT.RT", adjust="fdr") 
 head(topTab_KOvsWT.RT)
 
 
-## ---- topTabs3--------------------------------------------------------------
+## ---- topTabs3-----------------------------------------------------------------------------------------------------------------
 topTab_INT  <- topTable (fit.main, number=nrow(fit.main), coef="INT", adjust="fdr") 
 head(topTab_INT)
 
 
-## ----GeneAnnotation, message=FALSE, warning=FALSE---------------------------
+## ----GeneAnnotation, message=FALSE, warning=FALSE------------------------------------------------------------------------------
 annotatedTopTable <- function(topTab, anotPackage)
 {
   topTab <- cbind(PROBEID=rownames(topTab), topTab)
@@ -269,7 +269,7 @@ return(annotatedTopTab)
 }
 
 
-## ----annotateTopTables------------------------------------------------------
+## ----annotateTopTables---------------------------------------------------------------------------------------------------------
 topAnnotated_KOvsWT.COLD <- annotatedTopTable(topTab_KOvsWT.COLD,
 anotPackage="mogene21sttranscriptcluster.db")
 topAnnotated_KOvsWT.RT <- annotatedTopTable(topTab_KOvsWT.RT,
@@ -281,7 +281,7 @@ write.csv(topAnnotated_KOvsWT.RT, file="./results/topAnnotated_KOvsWT_RT.csv")
 write.csv(topAnnotated_INT, file="./results/topAnnotated_INT.csv")
 
 
-## ----annotatedTop, echo=FALSE-----------------------------------------------
+## ----annotatedTop, echo=FALSE--------------------------------------------------------------------------------------------------
 short<- head(topAnnotated_KOvsWT.COLD[1:5,1:4])
 # library(kableExtra)
 # knitr::kable(
@@ -300,7 +300,7 @@ volcanoplot(fit.main, coef=1, highlight=4, names=SYMBOLS,
   abline(v=c(-1,1))
 
 
-## ----saveVolcanos, echo=FALSE, results='hide'-------------------------------
+## ----saveVolcanos, echo=FALSE, results='hide'----------------------------------------------------------------------------------
 pdf("figures/Volcanos.pdf")
 for (i in colnames(cont.matrix)){
   volcanoplot(fit.main, coef=i, highlight=4, names=SYMBOLS,
@@ -310,23 +310,23 @@ for (i in colnames(cont.matrix)){
 dev.off()
 
 
-## ----decideTests.1----------------------------------------------------------
+## ----decideTests.1-------------------------------------------------------------------------------------------------------------
 library(limma)
 res<-decideTests(fit.main, method="separate", adjust.method="fdr", p.value=0.1, lfc=1)
 
 
-## ----resumeDecideTests------------------------------------------------------
+## ----resumeDecideTests---------------------------------------------------------------------------------------------------------
 sum.res.rows<-apply(abs(res),1,sum)
 res.selected<-res[sum.res.rows!=0,] 
 print(summary(res))
 
 
-## ---- vennDiagram, fig.cap="Venn diagram showing the genes in common between the three comparisons performed"----
+## ---- vennDiagram, fig.cap="Venn diagram showing the genes in common between the three comparisons performed"------------------
 vennDiagram (res.selected[,1:3], cex=0.9)
 title("Genes in common between the three comparisons\n Genes selected with FDR < 0.1 and logFC > 1")
 
 
-## ----data4Heatmap-----------------------------------------------------------
+## ----data4Heatmap--------------------------------------------------------------------------------------------------------------
 probesInHeatmap <- rownames(res.selected)
 HMdata <- exprs(eset_filtered)[rownames(exprs(eset_filtered)) %in% probesInHeatmap,]
 
@@ -336,7 +336,7 @@ rownames(HMdata) <- SYMBOLS
 write.csv(HMdata, file = file.path("./results/data4Heatmap.csv"))
 
 
-## ----heatmapNoclustering, fig.cap="Heatmap for expression data without any grouping"----
+## ----heatmapNoclustering, fig.cap="Heatmap for expression data without any grouping"-------------------------------------------
 my_palette <- colorRampPalette(c("blue", "red"))(n = 299)
 library(gplots)
 
@@ -380,7 +380,7 @@ heatmap.2(HMdata,
 
 
 
-## ----selectGenes------------------------------------------------------------
+## ----selectGenes---------------------------------------------------------------------------------------------------------------
 listOfTables <- list(KOvsWT.COLD = topTab_KOvsWT.COLD, 
                      KOvsWT.RT  = topTab_KOvsWT.RT, 
                      INT = topTab_INT)
@@ -400,13 +400,13 @@ for (i in 1:length(listOfTables)){
 sapply(listOfSelected, length)
 
 
-## ---------------------------------------------------------------------------
+## ------------------------------------------------------------------------------------------------------------------------------
 mapped_genes2GO <- mappedkeys(org.Mm.egGO)
 mapped_genes2KEGG <- mappedkeys(org.Mm.egPATH)
 mapped_genes <- union(mapped_genes2GO , mapped_genes2KEGG)
 
 
-## ----BiologicalSig----------------------------------------------------------
+## ----BiologicalSig-------------------------------------------------------------------------------------------------------------
 library(ReactomePA)
 
 listOfData <- listOfSelected[1:2]
@@ -450,7 +450,7 @@ for (i in 1:length(listOfData)){
          vertex.label.cex = 0.75)
 
 
-## ----tableReacto, echo=FALSE------------------------------------------------
+## ----tableReacto, echo=FALSE---------------------------------------------------------------------------------------------------
 Tab.react <- read.csv2(file.path("./results/ReactomePA.Results.KOvsWT.RT.csv"), 
                        sep = ",", header = TRUE, row.names = 1)
 
@@ -458,7 +458,7 @@ Tab.react <- Tab.react[1:4, 1:5]
 knitr::kable(Tab.react, booktabs = TRUE, caption = "First rows and columns for Reactome results on KOvsWT.RT.csv comparison")
 
 
-## ----listOfFiles, echo=FALSE------------------------------------------------
+## ----listOfFiles, echo=FALSE---------------------------------------------------------------------------------------------------
 listOfFiles <- dir("./results/") 
 knitr::kable(
   listOfFiles, booktabs = TRUE,
